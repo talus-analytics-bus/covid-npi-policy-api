ce2a5455286ad260cfc7b91df41887134d805816
mevanmaele <mvanmaele@talusanalytics.com>
2020-05-05 15:40:34 -0400

    logs

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ from db import db
 
 DO_INGEST = False
 if DO_INGEST:
-    print('ingesting data')
+    print('Ingesting data.')
     db.generate_mapping(check_tables=False, create_tables=False)
     db.drop_all_tables(with_all_data=True)
     db.create_tables()
     schema.ingest_covid_npi_policy()
 
 else:
-    print("generating mapping")
     db.generate_mapping()
