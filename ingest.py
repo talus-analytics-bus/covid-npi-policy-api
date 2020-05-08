42524e74851b2c349935e2d17ba1017250cdd0ef
mevanmaele <mvanmaele@talusanalytics.com>
2020-05-08 14:12:31 -0400

    separate ingest from main

---
+++ ingest.py
@@ -0,0 +1,14 @@
+"""Run data ingest application"""
+# standard modules
+from os import sys
+
+# local modules
+from api import schema
+from db import db
+
+print('Ingesting data.')
+db.generate_mapping(check_tables=False, create_tables=False)
+db.drop_all_tables(with_all_data=True)
+db.create_tables()
+schema.ingest_covid_npi_policy()
+sys.exit(0)
--- main.py
+++ main.py
@@ -3,13 +3,4 @@
 from api import app, schema
 from db import db
 
-DO_INGEST = False
-if DO_INGEST:
-    print('Ingesting data.')
-    db.generate_mapping(check_tables=False, create_tables=False)
-    db.drop_all_tables(with_all_data=True)
-    db.create_tables()
-    schema.ingest_covid_npi_policy()
-
-else:
-    db.generate_mapping()
+db.generate_mapping()
