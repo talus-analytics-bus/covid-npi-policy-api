7596baf54d94a474e8bfb4a1773750ab00263079
mevanmaele <mvanmaele@talusanalytics.com>
2020-05-11 12:34:27 -0400

    restore ingest zcript

--- ingest.py
+++ ingest.py
@@ -7,10 +7,9 @@ from api import schema
 from db import db
 
 print('Ingesting data.')
-# db.generate_mapping(check_tables=False, create_tables=False)
-db.generate_mapping()
-# db.drop_all_tables(with_all_data=True)
-# db.create_tables()
-# schema.ingest_covid_npi_policy()
-schema.clean_docs()
+db.generate_mapping(check_tables=False, create_tables=False)
+db.drop_all_tables(with_all_data=True)
+db.create_tables()
+schema.ingest_covid_npi_policy()
+# schema.clean_docs()
 sys.exit(0)
