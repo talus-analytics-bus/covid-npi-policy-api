3f9a1509b30d914a636df1007c203003950fe687
mevanmaele <mvanmaele@talusanalytics.com>
2020-05-22 15:57:20 -0400

    allow domain

--- api/app.py
+++ api/app.py
@@ -9,6 +9,7 @@ app = FastAPI()
 # set allowed origins
 allow_origin_regex = \
     "(http:\/\/localhost:.*|" + \
+    "https?:\/\/.*\.covidamp\.org|" + \
     "https?:\/\/.*\.cloudfront\.net|" + \
     "https?:\/\/.*\.talusanalytics.*)"
 
