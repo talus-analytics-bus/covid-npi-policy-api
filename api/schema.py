401a03861087e49e99e6f1079874fc5526f55487
mevanmaele <mvanmaele@talusanalytics.com>
2020-10-20 10:09:45 -0400

    Add commented out code that can replace "No restrictions" with "New normal" (this is done in the database for now)

--- api/schema.py
+++ api/schema.py
@@ -27,6 +27,7 @@ from db import db
 # constants
 s3 = boto3.client('s3')
 S3_BUCKET_NAME = 'covid-npi-policy-storage'
+# IMPLEMENTED_NO_RESTRICTIONS = False
 
 
 def cached(func):
@@ -832,6 +833,28 @@ def get_policy_status(
     return res
 
 
+# def parse_lockdown_level(value_tmp):
+#     """If "No restrictions" has not yet been implemented on the frontend, then
+#     return "new normal" instead of "no restrictions". Otherwise return the val.
+#
+#     Parameters
+#     ----------
+#     value_tmp : type
+#         Description of parameter `value_tmp`.
+#
+#     Returns
+#     -------
+#     type
+#         Description of returned object.
+#
+#     """
+#     if not IMPLEMENTED_NO_RESTRICTIONS:
+#         if value_tmp == 'No restrictions':
+#             return 'New normal'
+#         else:
+#             return value_tmp
+
+
 @cached
 @db_session
 def get_lockdown_level(
@@ -865,6 +888,7 @@ def get_lockdown_level(
         if country_only and i.place.level != 'Country':
             continue
         else:
+            # value = parse_lockdown_level(i.value)
             datum = {
                 'value': i.value,
                 'datestamp': i.date,
