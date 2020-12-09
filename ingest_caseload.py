6bdbe3cacff8565b6bcca12a517581920ff40b18
mevanmaele <mvanmaele@talusanalytics.com>
2020-12-09 10:30:35 -0500

    Add Python to refresh materialized views, instead of doing it with shell scripts

--- ingest_caseload.py
+++ ingest_caseload.py
@@ -9,6 +9,9 @@ from db_metric import db
 from db import db as db_amp
 from ingest import CovidCaseloadPlugin
 
+# 3rd party modules
+from pony.orm import db_session
+
 # setup arguments
 parser = argparse.ArgumentParser(description='Ingest caseload data')
 parser.add_argument('-s', '--state', default=False,
@@ -24,6 +27,34 @@ parser.add_argument('-a', '--all', default=False,
                     const=True,
                     help='ingest all data')
 
+
+@db_session
+def refresh_materialized_views():
+    """Refresh materialized views that depend on case/deaths data.
+
+    """
+
+    # get connection to DBAPI
+    conn = db.get_connection()
+    cur = conn.cursor()
+
+    # single statement to refresh all materialized views relevant
+    to_refresh = ['74', '77', '94']
+    # to_refresh = ['74', '77', '94', '97']  # 97 is 7d aggregate deaths, unused
+    stmt_list = list()
+    for id in to_refresh:
+        stmt = f'''REFRESH MATERIALIZED VIEW metric_{id}'''
+        stmt_list.append(stmt)
+    stmt_str = "; ".join(stmt_list)
+
+    print('\nRefreshing materialized views (x' + str(len(to_refresh)) + ')...')
+    then = time.perf_counter()
+    cur.execute(stmt_str)
+    now = time.perf_counter()
+    sec = now - then
+    print('Refreshed. Time elapsed: ' + str(sec))
+
+
 if __name__ == "__main__":
     # get args
     args = parser.parse_args()
@@ -35,15 +66,8 @@ if __name__ == "__main__":
     db_amp.generate_mapping(create_tables=False)
     plugin = CovidCaseloadPlugin()
     plugin.upsert_data(db, db_amp, do_state=do_state, do_global=do_global)
-    print('Success!')
 
-    # plugin.load_client().load_data().process_data(db)
+    # refresh materialized views that depend on case/deaths data
+    refresh_materialized_views()
+    print('\nData ingested.')
     sys.exit(0)
-
-    # # Drop all data/tables before ingesting
-    # db.generate_mapping(check_tables=False, create_tables=False)
-    # db.drop_all_tables(with_all_data=True)
-    # db.create_tables()
-    # plugin = CovidPolicyPlugin()
-    # plugin.load_client().load_data().process_data(db)
-    # sys.exit(0)
