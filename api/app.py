5f37784e20cd7b6157e5c6fd771e193418ef517b
mevanmaele <mvanmaele@talusanalytics.com>
2020-12-21 10:57:26 -0500

    Reorder and reorganize routes into tag groups, add description and title for API docs

--- api/app.py
+++ api/app.py
@@ -2,8 +2,39 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from fastapi.openapi.utils import get_openapi
 
 # define API app
+tags_metadata = [
+    {
+        "name": "Policies",
+        "description": "Operations to get data for policies and counts of policies in certain categories and/or subcategories.",
+    },
+    {
+        "name": "Plans",
+        "description": "Operations to get data for policies and counts of policies in certain categories and/or subcategories.",
+    },
+    {
+        "name": "Court challenges",
+        "description": "Operations to get data for policies and counts of policies in certain categories and/or subcategories.",
+    },
+    {
+        "name": "Distancing levels",
+        "description": "Operations to get data for policies and counts of policies in certain categories and/or subcategories.",
+    },
+    {
+        "name": "Metadata",
+        "description": "Operations to get metadata including field definitions, optionset values for dropdowns based on data fields, counts of policies, plans, or court challenges in the database, and the current version of different data types (i.e., date last updated).",
+        # "externalDocs": {
+        #     "description": "Items external docs",
+        #     "url": "https://fastapi.tiangolo.com/",
+        # },
+    },
+    {
+        "name": "Downloads",
+        "description": "Operations to download data. Excel-exportable data types can be downloaded with `/post/export` and include policies, plans, and court challenges. Filters may be applied. Individual PDF files associated with those data types can be downloaded using the `/get/file` endpoints.",
+    },
+]
 app = FastAPI()
 
 # set allowed origins
@@ -22,3 +53,25 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+def custom_openapi():
+    # if app.openapi_schema:
+    # return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="COVID AMP application programming interface (API) documentation",
+        version="1.0.0",
+        description="<p>The <strong>COVID Analysis and Mapping of Policies (AMP)</strong> site provides access to a comprehensive list of policies and plans implemented globally to address the COVID-19 pandemic. This API provides access to some of the underlying data used in the COVID AMP site.</p><p>You can visit the site at <a href=\"https://covidamp.org/\" target=\"_blank\">https://covidamp.org/</a>.</p>",
+        routes=app.routes
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://covidamp.org/static/media/logo.b7a0d643.svg",
+        "altText": "COVID AMP logo",
+        "href": "https://covidamp.org/"
+    }
+    openapi_schema["tags"] = tags_metadata
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
--- api/routing.py
+++ api/routing.py
@@ -15,8 +15,8 @@ from .app import app
 from db import db
 
 
-@app.post("/post/export")
-async def export(
+@app.post("/post/export", tags=["Downloads"])
+async def Export_to_Excel_file(
     body: PolicyFilters,
     class_name: str,
 ):
@@ -37,24 +37,24 @@ async def export(
     return schema.export(filters=filters, class_name=class_name)
 
 
-@app.get("/get/version")
+@app.get("/get/version", tags=["Metadata"])
 async def get_version():
     return schema.get_version()
 
 
-@app.get("/get/countries_with_lockdown_levels")
+@app.get("/get/countries_with_lockdown_levels", tags=["Metadata"])
 async def get_countries_with_lockdown_levels():
     return schema.get_countries_with_lockdown_levels()
 
 
-@app.get("/get/count")
+@app.get("/get/count", tags=["Metadata"])
 async def get_count(
     class_names: List[str] = Query(None),
 ):
     return schema.get_count(class_names=class_names)
 
 
-@app.get("/get/metadata", response_model=MetadataList)
+@app.get("/get/metadata", response_model=MetadataList, tags=["Metadata"])
 async def get_metadata(
     fields: List[str] = Query(None),
     entity_class_name: str = 'Policy'
@@ -78,7 +78,7 @@ async def get_metadata(
     )
 
 
-@app.get("/get/file/redirect")
+@app.get("/get/file/redirect", tags=["Downloads"])
 async def get_file_redirect(id: int):
     """Return file from S3 with the matching ID using the provided title.
 
@@ -99,7 +99,7 @@ async def get_file_redirect(id: int):
     return RedirectResponse(url=f'''/get/file/{title}?id={id}''')
 
 
-@app.get("/get/file/{title}")
+@app.get("/get/file/{title}", tags=["Downloads"])
 async def get_file(id: int, title: str):
     """Return file from S3 with the matching ID using the provided title.
 
@@ -119,7 +119,7 @@ async def get_file(id: int, title: str):
     return schema.get_file(id)
 
 
-@app.get("/get/policy", response_model=ListResponse, response_model_exclude_unset=True)
+@app.get("/get/policy", response_model=ListResponse, response_model_exclude_unset=True, tags=["Policies"], include_in_schema=False)
 async def get_policy(
     fields: List[str] = Query(None),
     page: int = None,
@@ -144,152 +144,7 @@ async def get_policy(
     )
 
 
-@app.get("/get/challenge", response_model=ListResponse, response_model_exclude_unset=True)
-async def get_challenge(
-    fields: List[str] = Query(None),
-    page: int = None,
-    pagesize: int = 100,
-):
-    """Return Court_Challenge data.
-
-    Parameters
-    ----------
-    fields : List[str]
-        Data fields to return.
-
-    Returns
-    -------
-    dict
-        Challenge response dictionary.
-
-    """
-    return schema.get_challenge(fields=fields, page=page, pagesize=pagesize)
-
-
-@app.get("/get/plan", response_model=ListResponse, response_model_exclude_unset=True)
-async def get_plan(
-    fields: List[str] = Query(None),
-    page: int = None,
-    pagesize: int = 100,
-):
-    """Return Plan data.
-
-    Parameters
-    ----------
-    fields : List[str]
-        Data fields to return.
-
-    Returns
-    -------
-    dict
-        Plan response dictionary.
-
-    """
-    return schema.get_plan(fields=fields, page=page, pagesize=pagesize)
-
-
-@app.get("/get/policy_status/{geo_res}", response_model=PolicyStatusList, response_model_exclude_unset=True)
-async def get_policy_status(geo_res=str):
-    """Return Policy data.
-
-    Parameters
-    ----------
-    fields : List[str]
-        Data fields to return.
-
-    Returns
-    -------
-    dict
-        Policy response dictionary.
-
-    """
-    return schema.get_policy_status(geo_res=geo_res)
-
-
-@app.get("/get/lockdown_level/model/{iso3}/{geo_res}/{name}/{end_date}", response_model=PolicyStatusList, response_model_exclude_unset=True)
-async def get_lockdown_level_model(
-    iso3=str,
-    geo_res=str,
-    end_date=str,
-    name=str,
-    deltas_only: bool = False
-):
-    """Get lockdown level of a location by date.
-
-    """
-    return schema.get_lockdown_level(
-        iso3=iso3,
-        geo_res=geo_res,
-        name=name,
-        end_date=end_date,
-        deltas_only=deltas_only,
-    )
-
-
-@app.get("/get/lockdown_level/country/{iso3}/{end_date}", response_model=PolicyStatusList, response_model_exclude_unset=True)
-async def get_lockdown_level_country(
-    iso3=str,
-    end_date=str,
-    deltas_only: bool = False
-):
-    """Get lockdown level of a location by date.
-
-    """
-    return schema.get_lockdown_level(
-        iso3=iso3,
-        geo_res='country',
-        end_date=end_date,
-        deltas_only=deltas_only
-    )
-
-
-@app.get("/get/lockdown_level/map/{iso3}/{geo_res}/{date}", response_model=PolicyStatusList, response_model_exclude_unset=True)
-async def get_lockdown_level_map(iso3=str, geo_res=str, date=date):
-    """Get lockdown level of a location by date.
-
-    """
-    return schema.get_lockdown_level(iso3=iso3, geo_res=geo_res, date=date)
-
-
-@app.post("/post/policy_status/{geo_res}", response_model=PolicyStatusList, response_model_exclude_unset=True)
-async def post_policy_status(body: PolicyFilters, geo_res=str):
-    """Return Policy data.
-
-    Parameters
-    ----------
-    fields : List[str]
-        Data fields to return.
-
-    Returns
-    -------
-    dict
-        Policy response dictionary.
-
-    """
-
-    return schema.get_policy_status(geo_res=geo_res, filters=body.filters)
-
-
-@app.post("/post/policy_status_counts/{geo_res}", response_model=PolicyStatusCountList, response_model_exclude_unset=True)
-async def post_policy_status(body: PolicyFilters, geo_res=str):
-    """Return Policy status counts.
-
-    Parameters
-    ----------
-    fields : List[str]
-        Data fields to return.
-
-    Returns
-    -------
-    dict
-        Policy response dictionary.
-
-    """
-    res = schema.get_policy_status_counts(geo_res=geo_res, filters=body.filters)
-    return res
-
-
-@app.post("/post/policy", response_model=ListResponse, response_model_exclude_unset=True)
+@app.post("/post/policy", response_model=ListResponse, response_model_exclude_unset=True, tags=["Policies"])
 async def post_policy(
     body: PolicyFilters,
     by_category: str = None,
@@ -320,7 +175,152 @@ async def post_policy(
     )
 
 
-@app.post("/post/policy_number", response_model=ListResponse, response_model_exclude_unset=True)
+@app.get("/get/challenge", response_model=ListResponse, response_model_exclude_unset=True, tags=["Court challenges"], include_in_schema=False)
+async def get_challenge(
+    fields: List[str] = Query(None),
+    page: int = None,
+    pagesize: int = 100,
+):
+    """Return Court_Challenge data.
+
+    Parameters
+    ----------
+    fields : List[str]
+        Data fields to return.
+
+    Returns
+    -------
+    dict
+        Challenge response dictionary.
+
+    """
+    return schema.get_challenge(fields=fields, page=page, pagesize=pagesize)
+
+
+@app.get("/get/plan", response_model=ListResponse, response_model_exclude_unset=True, tags=["Plans"], include_in_schema=False)
+async def get_plan(
+    fields: List[str] = Query(None),
+    page: int = None,
+    pagesize: int = 100,
+):
+    """Return Plan data.
+
+    Parameters
+    ----------
+    fields : List[str]
+        Data fields to return.
+
+    Returns
+    -------
+    dict
+        Plan response dictionary.
+
+    """
+    return schema.get_plan(fields=fields, page=page, pagesize=pagesize)
+
+
+@app.get("/get/policy_status/{geo_res}", response_model=PolicyStatusList, response_model_exclude_unset=True, tags=["Policies"], include_in_schema=False)
+async def get_policy_status(geo_res=str):
+    """Return Policy data.
+
+    Parameters
+    ----------
+    fields : List[str]
+        Data fields to return.
+
+    Returns
+    -------
+    dict
+        Policy response dictionary.
+
+    """
+    return schema.get_policy_status(geo_res=geo_res)
+
+
+@app.get("/get/lockdown_level/model/{iso3}/{geo_res}/{name}/{end_date}", response_model=PolicyStatusList, response_model_exclude_unset=True, tags=["Distancing levels"])
+async def get_lockdown_level_model(
+    iso3=str,
+    geo_res=str,
+    end_date=str,
+    name=str,
+    deltas_only: bool = False
+):
+    """Get lockdown level of a location by date.
+
+    """
+    return schema.get_lockdown_level(
+        iso3=iso3,
+        geo_res=geo_res,
+        name=name,
+        end_date=end_date,
+        deltas_only=deltas_only,
+    )
+
+
+@app.get("/get/lockdown_level/country/{iso3}/{end_date}", response_model=PolicyStatusList, response_model_exclude_unset=True, tags=["Distancing levels"])
+async def get_lockdown_level_country(
+    iso3=str,
+    end_date=str,
+    deltas_only: bool = False
+):
+    """Get lockdown level of a location by date.
+
+    """
+    return schema.get_lockdown_level(
+        iso3=iso3,
+        geo_res='country',
+        end_date=end_date,
+        deltas_only=deltas_only
+    )
+
+
+@app.get("/get/lockdown_level/map/{iso3}/{geo_res}/{date}", response_model=PolicyStatusList, response_model_exclude_unset=True, tags=["Distancing levels"])
+async def get_lockdown_level_map(iso3=str, geo_res=str, date=date):
+    """Get lockdown level of a location by date.
+
+    """
+    return schema.get_lockdown_level(iso3=iso3, geo_res=geo_res, date=date)
+
+
+@app.post("/post/policy_status/{geo_res}", response_model=PolicyStatusList, response_model_exclude_unset=True, tags=["Policies"])
+async def post_policy_status(body: PolicyFilters, geo_res=str):
+    """Return Policy data.
+
+    Parameters
+    ----------
+    fields : List[str]
+        Data fields to return.
+
+    Returns
+    -------
+    dict
+        Policy response dictionary.
+
+    """
+
+    return schema.get_policy_status(geo_res=geo_res, filters=body.filters)
+
+
+@app.post("/post/policy_status_counts/{geo_res}", response_model=PolicyStatusCountList, response_model_exclude_unset=True, tags=["Policies"])
+async def post_policy_status(body: PolicyFilters, geo_res=str):
+    """Return Policy status counts.
+
+    Parameters
+    ----------
+    fields : List[str]
+        Data fields to return.
+
+    Returns
+    -------
+    dict
+        Policy response dictionary.
+
+    """
+    res = schema.get_policy_status_counts(geo_res=geo_res, filters=body.filters)
+    return res
+
+
+@app.post("/post/policy_number", response_model=ListResponse, response_model_exclude_unset=True, include_in_schema=False, tags=["Advanced"])
 async def post_policy_number(
     body: PolicyFilters,
     by_category: str = None,
@@ -337,7 +337,7 @@ async def post_policy_number(
     )
 
 
-@app.post("/post/challenge", response_model=ListResponse, response_model_exclude_unset=True)
+@app.post("/post/challenge", response_model=ListResponse, response_model_exclude_unset=True, tags=["Court challenges"])
 async def post_challenge(
     body: PolicyFilters,
     by_category: str = None,
@@ -366,7 +366,7 @@ async def post_challenge(
     )
 
 
-@app.post("/post/plan", response_model=ListResponse, response_model_exclude_unset=True)
+@app.post("/post/plan", response_model=ListResponse, response_model_exclude_unset=True, tags=["Plans"])
 async def post_plan(
     body: PolicyFilters,
     by_category: str = None,
@@ -395,7 +395,7 @@ async def post_plan(
     )
 
 
-@app.get("/get/optionset", response_model=OptionSetList)
+@app.get("/get/optionset", response_model=OptionSetList, tags=["Metadata"])
 async def get_optionset(
     fields: List[str] = Query(None),
     class_name: str = 'Policy'
@@ -425,7 +425,7 @@ async def get_optionset(
 ##
 # Test endpoints
 ##
-@app.get("/test")
+@app.get("/test", include_in_schema=False)
 async def get_test(test_param: str = 'GET successful'):
     """Test GET endpoint.
 
@@ -443,26 +443,26 @@ async def get_test(test_param: str = 'GET successful'):
     """
     return [{'success': True, 'message': 'GET test', 'data': [test_param]}]
 
-##
-# Debug endpoints
-##
-
-
-@app.get("/add_search_text")
-async def add_search_text(test_param: str = 'GET successful'):
-    """Test GET endpoint.
-
-    Parameters
-    ----------
-    test_param : str
-        A message to be returned in the response if GET was successful.
-
-    Returns
-    -------
-    list[dict]
-        A message containing the value of `test_param` indicating the GET was
-        successful.
-
-    """
-    schema.add_search_text()
-    return 'Done'
+# ##
+# # Debug endpoints
+# ##
+#
+#
+# @app.get("/add_search_text")
+# async def add_search_text(test_param: str = 'GET successful'):
+#     """Test GET endpoint.
+#
+#     Parameters
+#     ----------
+#     test_param : str
+#         A message to be returned in the response if GET was successful.
+#
+#     Returns
+#     -------
+#     list[dict]
+#         A message containing the value of `test_param` indicating the GET was
+#         successful.
+#
+#     """
+#     schema.add_search_text()
+#     return 'Done'
