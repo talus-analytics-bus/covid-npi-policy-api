f2c6870cfd4401ece497a03e9b90ae37347f0743
mvanmaele <mvanmaele@talusanalytics.com>
2021-05-17 18:29:17 -0400

    Update debug file

--- debug.py
+++ debug.py
@@ -3,21 +3,7 @@
 # import pandas as pd
 
 # local modules
-from db import db
-from ingest.plugins import CovidPolicyPlugin
-
+from api.ampresolvers.tests import test_policy_status_counter
 
 if __name__ == "__main__":
-    # define red and green airtable keys and pick the one to use
-    airtable_key = "appoXaOlIgpiHK3I2"
-
-    # generate db mapping
-    db.generate_mapping(create_tables=True)
-
-    # update core policy data, if appropriate
-    plugin = CovidPolicyPlugin()
-    client = plugin.load_client(airtable_key)
-    client.load_data().process_data(db).post_process_policies(
-        db
-    ).post_process_policy_numbers(db)
-    plugin.assign_policy_group_numbers(db)
+    test_policy_status_counter.test_countries()
