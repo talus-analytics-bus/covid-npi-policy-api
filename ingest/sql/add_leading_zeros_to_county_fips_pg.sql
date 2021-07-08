01a3a76a08e9f5eabe2ea5471b8c9d7b3acbb2d1
mvanmaele <mvanmaele@talusanalytics.com>
2021-07-08 14:38:58 -0400

    Remove unused files

--- ingest/sql/add_leading_zeros_to_county_fips_pg.sql
+++
@@ -1,8 +0,0 @@
-update place
-set ansi_fips = '0' || ansi_fips
-where level = 'Local plus state/province'
-    and length(ansi_fips) = 4;
-update place
-set ansi_fips = '0' || ansi_fips
-where level = 'Local'
-    and length(ansi_fips) = 4
