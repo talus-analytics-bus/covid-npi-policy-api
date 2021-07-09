9a2b67cdfc16c83dd75b41f3dd89b316619686a0
mvanmaele <mvanmaele@talusanalytics.com>
2021-07-09 17:37:10 -0400

    Make delete script much more efficient

--- ingest/sql/delete_policies_without_places_pg.sql
+++ ingest/sql/delete_policies_without_places_pg.sql
@@ -1,5 +1,11 @@
+with policy_to_delete as (
+    select id
+    from policy p
+        left join place_to_policy p2p on p2p.policy = p.id
+    where p2p.policy is null
+)
 delete from policy
-where id not in (
-        select policy
-        from place_to_policy
+where id in (
+        select id
+        from policy_to_delete
     );
