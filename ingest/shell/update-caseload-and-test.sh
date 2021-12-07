92e12eec5bd51bb54eaf67d7592c1b6f69446580
mvanmaele <mvanmaele@talusanalytics.com>
2021-12-07 09:53:08 -0500

    Add script flag to skip caseload data update

--- ingest/shell/update-caseload-and-test.sh
+++ ingest/shell/update-caseload-and-test.sh
@@ -7,24 +7,32 @@ programname=$0
 
 function usage {
     echo "usage: $programname [username]"
-    echo "  username        your local pgsql server username"
-    echo "  dblocal         the name of the COVID AMP database on your local server with which you are updating"
-    echo "  dbmetriclocal   the name of the Metrics database on your local server with which you are updating"
-    echo "  dbprodhost      the host name of the production server to which you are updating"
-    echo "  airtablekey     your Airtable API key"
+    echo "  username            your local pgsql server username"
+    echo "  dblocal             the name of the COVID AMP database on your local server with which you are updating"
+    echo "  dbmetriclocal       the name of the Metrics database on your local server with which you are updating"
+    echo "  dbprodhost          the host name of the production server to which you are updating"
+    echo "  airtablekey         your Airtable API key"
+    echo "  skip_caseload_data  true if caseload data should not be updated (faster)"
     exit 1
 }
 
 now=$(date);
+echo Beginning update at time $(date)
 username=${1?Provide your local pgsql server username in first argument};
 dblocal=${2?Provide the name of the database on your local server with which you are updating in second argument};
 dbmetriclocal=${3?Provide the name of the database on your local server with which you are updating in third argument};
 dbprodhost=${4?Provide the name of the database on your local server with which you are updating in fourth argument};
 airtablekey=${5?Provide your Airtable API key in fifth argument};
+skip_caseload_data=${6-false}
 
-echo Ingesting data and updating test site...;
+if [ "$skip_caseload_data" = false ] ; then
+    echo 'Updating caseload data...'
+    bash ingest/sql/ingest-caseload.sh $username $dblocal && \
+    bash ingest/shell/update-metric-prod-from-local.sh $username $dbmetriclocal $dbprodhost && \  
+else
+    echo 'Not updating caseload data.'
+fi
 export AIRTABLE_API_KEY=$airtablekey;
-bash ingest/sql/ingest-caseload.sh $username $dblocal && \
-bash ingest/shell/update-metric-prod-from-local.sh $username $dbmetriclocal $dbprodhost && \  
 bash ingest/shell/ingest.sh $username $dblocal && \
-bash ingest/shell/update-test-from-local.sh $username $dblocal $dbprodhost;
+bash ingest/shell/update-test-from-local.sh $username $dblocal $dbprodhost && \
+echo Completed update at time $(date)
