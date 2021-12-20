2fbea49e1128df4384942a4fdb640aac099a867d
mvanmaele <mvanmaele@talusanalytics.com>
2021-12-20 13:06:03 -0500

    Add static XLSX shell script to chain

--- ingest/shell/update-caseload-and-test.sh
+++ ingest/shell/update-caseload-and-test.sh
@@ -35,4 +35,5 @@ fi
 export AIRTABLE_API_KEY=$airtablekey;
 bash ingest/shell/ingest.sh $username $dblocal && \
 bash ingest/shell/update-test-from-local.sh $username $dblocal $dbprodhost && \
+bash ingest/shell/update-static-xlsx-files.sh && \
 echo Completed update at time $(date)
