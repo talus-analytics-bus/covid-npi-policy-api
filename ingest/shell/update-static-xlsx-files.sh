44a735f0e26f86f6eeadee04b06fba7ad5897279
mvanmaele <mvanmaele@talusanalytics.com>
2021-12-20 12:55:16 -0500

    Add shell script for XLSX downloads

---
+++ ingest/shell/update-static-xlsx-files.sh
@@ -0,0 +1,17 @@
+DEFAULT_PATH="../gida-landing/"
+STATIC_XLSX_REL_PATH="${1:-$DEFAULT_PATH}"
+BASEDIR=$(dirname $0)
+ORIGDIR=${PWD}
+uvicorn main:app --port 8002 &
+pid=$!
+echo PID is $pid
+sleep 15;
+cd $BASEDIR && \
+cd ../../ && \
+cd $STATIC_XLSX_REL_PATH && \
+bash ./shell/update-amp.sh && \
+bash ./shell/update-amp-summary.sh && \
+npm run deploy;
+kill $pid && \
+cd $ORIGDIR && \
+exit 0;
