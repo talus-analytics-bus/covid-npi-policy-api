51a0f48b8e61bf72b6e461c569336b8c858bbbe3
mvanmaele <mvanmaele@talusanalytics.com>
2022-05-27 14:20:03 -0400

    Add confirmation to `distancing` command

--- cli/ingest/distancing.py
+++ cli/ingest/distancing.py
@@ -2,13 +2,26 @@ import logging
 
 import click
 
+from cli import options
+
 logger = logging.getLogger(__name__)
 
 
-@click.command(help="Ingest distancing levels from S3 bucket")
-def distancing():
-    from ingest.distancinglevelgetter.core import DistancingLevelGetter
+@click.command(
+    help="Ingest distancing levels from S3 bucket `s3://covid-npi-policy-storage`."
+)
+@options.yes
+def distancing(yes: bool):
+    if not yes:
+        click.confirm(
+            "This will replace all distancing levels in table `observation` of the"
+            " database.\nDo you want to continue?",
+            abort=True,
+        )
+
+    # import modules
     from db import db
+    from ingest.distancinglevelgetter.core import DistancingLevelGetter
 
     # Update observations of distancing levels
     try:
